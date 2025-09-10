85201351d5281f341e5c6dc5b35bb04eeea4c125
Sebastian Mena <smf-mena@hotmail.com>
2025-09-10 13:37:54 -0500

    cors fixed

--- app/router/middleware/cors.go
+++ app/router/middleware/cors.go
@@ -10,8 +10,8 @@ func CORS(allowedOrigins []string) Middleware {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			origin := r.Header.Get("Origin")
 			if slices.Contains(allowedOrigins, origin) {
-					w.Header().Set("Access-Control-Allow-Origin", origin)
-				}
+                w.Header().Set("Access-Control-Allow-Origin", origin)
+            }
 
 			w.Header().Set("Access-Control-Allow-Origin", "*")
 			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, DELETE, PUT")
