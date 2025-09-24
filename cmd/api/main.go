6da9ed7255725d33c88e803e0a5fe6638266a981
Sebastian Mena <smf-mena@hotmail.com>
2025-09-24 14:17:53 -0500

    Feature/tamers/tam 12 (#8)
    * some comments added

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -56,6 +56,7 @@ func main() {
     // Init background job to fetch remaining stocks
     // TODO: add retry pattern for getting stocks
     // TODO: circuit breaker pattern could work
+	// another comment
     go stocksFetcherService.FetchRemainingStocks(ctx, cancel, lastPage)
 
 	// TODO: add graceful shutdown
